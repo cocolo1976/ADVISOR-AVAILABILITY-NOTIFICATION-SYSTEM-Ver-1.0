ee595e814d51d99bda3cd88c545b23f4ac4dd596
hborg004 <hborg004@fiu.edu>
2016-11-12 20:46:42 -0500

    fixes to a bug in desk

--- delete.php
+++ delete.php
@@ -10,16 +10,29 @@ Removes a student from the qeueue and alerts the advisor
 // connect to the database
 
 include('sqliConnect.php');
-if (isset($_GET['id']) && is_numeric($_GET['id'])&&($_POST['formSubmit']))
+//$form =  $_GET['id2'];
+echo $_POST['formSubmit'];
+
+//echo $_GET['id2'];
+
+if (($_POST['formSubmit']!=NULL))
 {
+    //the id of the walkk in bieng removed form the queue and snet to advisor
+    $walkID = $_GET['id'];
+    //$walkID =$_POST['formSubmit'];
+   echo $walkID;
     //code to prevent adivsor from getting astudent if the status is not correct
-     $advisorID =  $_POST['formStatus'];
-     //echo $advisorID;
+    //Id of the advisor selected to receive the student
+    $advisorID = filter_input(INPUT_POST, "formStatus");
+    // $advisorID = $_POST['formStatus'];
+     
+    echo $advisorID;
+     
      $con = get_sqli();
      mysqli_select_db($con,"login");
 $sql="SELECT * FROM `login_details` WHERE id = '$advisorID'";
 $result3 = mysqli_query($con,$sql);
-//echo $result3;
+
 if (!$result3) {
     printf("Error: %s\n", mysqli_error($con));
     printf("result not working");
@@ -28,8 +41,9 @@ if (!$result3) {
   $row3 = mysqli_fetch_array($result3);
  
   $status3 =  $row3['Status'];
-   echo $status3; 
+  echo $status3; 
    
+   mysqli_close($con);
    
     //if ADVISOR IS READY FOR APOITMENT CONTINUE IF NOT DO RETuRN TO DESK PAGE
     if($status3 =='RFA'|| $status3 =='RFW')
@@ -39,7 +53,9 @@ if (!$result3) {
     //get the id
     //
    // $advisor = $_GET['advisor'];
-$id = $_GET['id'];
+    
+
+
 // get results from database
 
 if (!$con) {
@@ -47,13 +63,15 @@ if (!$con) {
 }
 //execute delete query
 mysqli_select_db($con,"login");
-$sql="DELETE FROM walk_in WHERE id=$id";
+$sql="DELETE FROM walk_in WHERE id=$walkID";
 $result = mysqli_query($con,$sql);
 
 if (!$result) {
     printf("Error: %s\n", mysqli_error($con));
     exit();
 }
+
+
 //gets the adviser from the drop down list and uses it to say a student is on the way
 
 	
@@ -61,10 +79,10 @@ if (!$result) {
 //execute delete query
 mysqli_select_db($con,"login");
 $sql="UPDATE `login_details` SET `student_sent` = '1' WHERE `login_details`.`id` = '$advisorID'";
-$result = mysqli_query($con,$sql);
+$result4 = mysqli_query($con,$sql);
 
 //echo $advisor;
-
+mysqli_close($con);
         
  
     } 
